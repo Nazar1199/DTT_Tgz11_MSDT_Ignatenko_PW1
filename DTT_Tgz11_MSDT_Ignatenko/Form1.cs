5d273f23326c42052aed0deb0fd14bd08bc99198
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-09-18 23:21:08 +0300

    feat: completed

--- DTT_Tgz11_MSDT_Ignatenko/Form1.cs
+++ DTT_Tgz11_MSDT_Ignatenko/Form1.cs
@@ -9,7 +9,13 @@ namespace DTT_Tgz11_MSDT_Ignatenko
 
 		private void buttonRes_Click(object sender, EventArgs e)
 		{
-
+			int x = int.Parse(textBoxX.Text);
+			double lSide = Math.Pow(x, 5);
+			double rSide = Math.Pow(x, 3);
+			double tan = Math.Tan((rSide*2)*(Math.PI/180));
+			double ctan = 1 / tan;
+			double result = lSide * ctan;
+			textBoxRes.Text = result.ToString();
 		}
 	}
 }
