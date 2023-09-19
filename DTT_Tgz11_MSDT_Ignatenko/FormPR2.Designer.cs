e487f100b7d14557d2ef35ffbbfa865985000966
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-09-20 00:44:02 +0300

    feat: added new form

---
+++ DTT_Tgz11_MSDT_Ignatenko/FormPR2.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace DTT_Tgz11_MSDT_Ignatenko
+{
+	partial class FormPR2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPR2));
+			pictureBox1 = new PictureBox();
+			buttonRes = new Button();
+			label2 = new Label();
+			label1 = new Label();
+			textBoxRes = new TextBox();
+			textBoxX = new TextBox();
+			textBox1 = new TextBox();
+			label3 = new Label();
+			groupBox1 = new GroupBox();
+			groupBox2 = new GroupBox();
+			radioButton1 = new RadioButton();
+			radioButton2 = new RadioButton();
+			radioButton3 = new RadioButton();
+			radioButton4 = new RadioButton();
+			label4 = new Label();
+			numericUpDown1 = new NumericUpDown();
+			groupBox3 = new GroupBox();
+			radioButton5 = new RadioButton();
+			radioButton6 = new RadioButton();
+			radioButton7 = new RadioButton();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			groupBox1.SuspendLayout();
+			groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+			groupBox3.SuspendLayout();
+			SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.BackgroundImageLayout = ImageLayout.None;
+			pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+			pictureBox1.Location = new Point(12, 12);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(152, 48);
+			pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+			pictureBox1.TabIndex = 1;
+			pictureBox1.TabStop = false;
+			// 
+			// buttonRes
+			// 
+			buttonRes.Location = new Point(57, 118);
+			buttonRes.Name = "buttonRes";
+			buttonRes.Size = new Size(100, 47);
+			buttonRes.TabIndex = 8;
+			buttonRes.Text = "Рассчитать результат";
+			buttonRes.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+			label2.Location = new Point(9, 92);
+			label2.Name = "label2";
+			label2.Size = new Size(43, 20);
+			label2.TabIndex = 6;
+			label2.Text = "Xh =";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+			label1.Location = new Point(9, 59);
+			label1.Name = "label1";
+			label1.Size = new Size(42, 20);
+			label1.TabIndex = 7;
+			label1.Text = "Xk =";
+			// 
+			// textBoxRes
+			// 
+			textBoxRes.Location = new Point(57, 89);
+			textBoxRes.Name = "textBoxRes";
+			textBoxRes.Size = new Size(100, 23);
+			textBoxRes.TabIndex = 4;
+			// 
+			// textBoxX
+			// 
+			textBoxX.Location = new Point(57, 60);
+			textBoxX.Name = "textBoxX";
+			textBoxX.Size = new Size(100, 23);
+			textBoxX.TabIndex = 5;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(57, 31);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(100, 23);
+			textBox1.TabIndex = 5;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+			label3.Location = new Point(9, 30);
+			label3.Name = "label3";
+			label3.Size = new Size(43, 20);
+			label3.TabIndex = 7;
+			label3.Text = "Xn =";
+			// 
+			// groupBox1
+			// 
+			groupBox1.Controls.Add(label3);
+			groupBox1.Controls.Add(buttonRes);
+			groupBox1.Controls.Add(textBoxX);
+			groupBox1.Controls.Add(label2);
+			groupBox1.Controls.Add(textBox1);
+			groupBox1.Controls.Add(textBoxRes);
+			groupBox1.Controls.Add(label1);
+			groupBox1.Location = new Point(12, 75);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new Size(169, 176);
+			groupBox1.TabIndex = 9;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Входные данные";
+			// 
+			// groupBox2
+			// 
+			groupBox2.Controls.Add(numericUpDown1);
+			groupBox2.Controls.Add(label4);
+			groupBox2.Controls.Add(radioButton4);
+			groupBox2.Controls.Add(radioButton3);
+			groupBox2.Controls.Add(radioButton2);
+			groupBox2.Controls.Add(radioButton1);
+			groupBox2.Location = new Point(12, 257);
+			groupBox2.Name = "groupBox2";
+			groupBox2.Size = new Size(169, 157);
+			groupBox2.TabIndex = 10;
+			groupBox2.TabStop = false;
+			groupBox2.Text = "Линия";
+			// 
+			// radioButton1
+			// 
+			radioButton1.AutoSize = true;
+			radioButton1.Location = new Point(6, 22);
+			radioButton1.Name = "radioButton1";
+			radioButton1.Size = new Size(94, 19);
+			radioButton1.TabIndex = 0;
+			radioButton1.TabStop = true;
+			radioButton1.Text = "radioButton1";
+			radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			radioButton2.AutoSize = true;
+			radioButton2.Location = new Point(6, 47);
+			radioButton2.Name = "radioButton2";
+			radioButton2.Size = new Size(94, 19);
+			radioButton2.TabIndex = 0;
+			radioButton2.TabStop = true;
+			radioButton2.Text = "radioButton1";
+			radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			radioButton3.AutoSize = true;
+			radioButton3.Location = new Point(6, 72);
+			radioButton3.Name = "radioButton3";
+			radioButton3.Size = new Size(94, 19);
+			radioButton3.TabIndex = 0;
+			radioButton3.TabStop = true;
+			radioButton3.Text = "radioButton1";
+			radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			radioButton4.AutoSize = true;
+			radioButton4.Location = new Point(6, 97);
+			radioButton4.Name = "radioButton4";
+			radioButton4.Size = new Size(94, 19);
+			radioButton4.TabIndex = 0;
+			radioButton4.TabStop = true;
+			radioButton4.Text = "radioButton1";
+			radioButton4.UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(6, 121);
+			label4.Name = "label4";
+			label4.Size = new Size(58, 15);
+			label4.TabIndex = 1;
+			label4.Text = "Толщина";
+			// 
+			// numericUpDown1
+			// 
+			numericUpDown1.Location = new Point(70, 119);
+			numericUpDown1.Name = "numericUpDown1";
+			numericUpDown1.Size = new Size(41, 23);
+			numericUpDown1.TabIndex = 2;
+			// 
+			// groupBox3
+			// 
+			groupBox3.Controls.Add(radioButton7);
+			groupBox3.Controls.Add(radioButton6);
+			groupBox3.Controls.Add(radioButton5);
+			groupBox3.Location = new Point(12, 420);
+			groupBox3.Name = "groupBox3";
+			groupBox3.Size = new Size(169, 111);
+			groupBox3.TabIndex = 11;
+			groupBox3.TabStop = false;
+			groupBox3.Text = "Тип графика";
+			// 
+			// radioButton5
+			// 
+			radioButton5.AutoSize = true;
+			radioButton5.Location = new Point(6, 22);
+			radioButton5.Name = "radioButton5";
+			radioButton5.Size = new Size(94, 19);
+			radioButton5.TabIndex = 0;
+			radioButton5.TabStop = true;
+			radioButton5.Text = "radioButton5";
+			radioButton5.UseVisualStyleBackColor = true;
+			// 
+			// radioButton6
+			// 
+			radioButton6.AutoSize = true;
+			radioButton6.Location = new Point(6, 47);
+			radioButton6.Name = "radioButton6";
+			radioButton6.Size = new Size(94, 19);
+			radioButton6.TabIndex = 1;
+			radioButton6.TabStop = true;
+			radioButton6.Text = "radioButton6";
+			radioButton6.UseVisualStyleBackColor = true;
+			// 
+			// radioButton7
+			// 
+			radioButton7.AutoSize = true;
+			radioButton7.Location = new Point(6, 72);
+			radioButton7.Name = "radioButton7";
+			radioButton7.Size = new Size(94, 19);
+			radioButton7.TabIndex = 1;
+			radioButton7.TabStop = true;
+			radioButton7.Text = "radioButton6";
+			radioButton7.UseVisualStyleBackColor = true;
+			// 
+			// FormPR2
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(727, 546);
+			Controls.Add(groupBox3);
+			Controls.Add(groupBox2);
+			Controls.Add(groupBox1);
+			Controls.Add(pictureBox1);
+			Name = "FormPR2";
+			Text = "Графмк Игнатенко Н.А.";
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			groupBox1.ResumeLayout(false);
+			groupBox1.PerformLayout();
+			groupBox2.ResumeLayout(false);
+			groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+			groupBox3.ResumeLayout(false);
+			groupBox3.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private PictureBox pictureBox1;
+		private Button buttonRes;
+		private Label label2;
+		private Label label1;
+		private TextBox textBoxRes;
+		private TextBox textBoxX;
+		private TextBox textBox1;
+		private Label label3;
+		private GroupBox groupBox1;
+		private GroupBox groupBox2;
+		private NumericUpDown numericUpDown1;
+		private Label label4;
+		private RadioButton radioButton4;
+		private RadioButton radioButton3;
+		private RadioButton radioButton2;
+		private RadioButton radioButton1;
+		private GroupBox groupBox3;
+		private RadioButton radioButton7;
+		private RadioButton radioButton6;
+		private RadioButton radioButton5;
+	}
+}
---
+++ DTT_Tgz11_MSDT_Ignatenko/FormPR2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DTT_Tgz11_MSDT_Ignatenko
+{
+	public partial class FormPR2 : Form
+	{
+		public FormPR2()
+		{
+			InitializeComponent();
+		}
+	}
+}
